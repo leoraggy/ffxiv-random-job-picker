abac9ff65e9bc3dcc6b4a903c026f891d3335355
leoraggy <leoragual35@gmail.com>
2025-10-18 23:10:15 -0700

    Initial commit

---
+++ js/data.js
@@ -0,0 +1,55 @@
+const jobData = {
+  pld: "pld.png",
+  war: "war.png",
+  drk: "drk.png",
+  gnb: "gnb.png",
+  whm: "whm.png",
+  sch: "sch.png",
+  ast: "ast.png",
+  sge: "sge.png",
+  mnk: "mnk.png",
+  drg: "drg.png",
+  nin: "nin.png",
+  sam: "sam.png",
+  rpr: "rpr.png",
+  vpr: "vpr.png",
+  brd: "brd.png",
+  mch: "mch.png",
+  dnc: "dnc.png",
+  blm: "blm.png",
+  smn: "smn.png",
+  rdm: "rdm.png",
+  pct: "pct.png",
+};
+
+const jobTitle = {
+  pld: "Paladin",
+  war: "Warrior",
+  drk: "Dark Knight",
+  gnb: "Gunbreaker",
+  whm: "White Mage",
+  sch: "Scholar",
+  ast: "Astrologian",
+  sge: "Sage",
+  mnk: "Monk",
+  drg: "Dragoon",
+  nin: "Ninja",
+  sam: "Samurai",
+  rpr: "Reaper",
+  vpr: "Viper",
+  brd: "Bard",
+  mch: "Machinist",
+  dnc: "Dancer",
+  blm: "Black Mage",
+  smn: "Summoner",
+  rdm: "Red Mage",
+  pct: "Pictomancer",
+};
+
+const tankJobs = ["pld", "war", "drk", "gnb"];
+const healerJobs = ["whm", "sch", "ast", "sge"];
+const meleeJobs = ["mnk", "drg", "nin", "sam", "rpr", "vpr"];
+const physRangedJobs = ["brd", "mch", "dnc"];
+const casterJobs = ["blm", "smn", "rdm", "pct"];
+
+let jobList = Object.keys(jobData);
---
+++ js/utils.js
@@ -0,0 +1,66 @@
+function addJobGroup(jobCodes) {
+  jobCodes.forEach((jobCode) => {
+    document.getElementById(jobCode).style.filter = "grayscale(0%)";
+    if (!jobList.includes(jobCode)) {
+      jobList.push(jobCode);
+    }
+  });
+}
+
+function allTanks() {
+  addJobGroup(tankJobs);
+}
+
+function allHealers() {
+  addJobGroup(healerJobs);
+}
+
+function allMelee() {
+  addJobGroup(meleeJobs);
+}
+
+function allRanged() {
+  addJobGroup(physRangedJobs);
+}
+
+function allMages() {
+  addJobGroup(casterJobs);
+}
+
+function toggleJob(jobCode) {
+  if (jobList.includes(jobCode)) {
+    jobList = jobList.filter((job) => job !== jobCode);
+    document.getElementById(jobCode).style.filter = "grayscale(100%)";
+  } else {
+    jobList.push(jobCode);
+    document.getElementById(jobCode).style.filter = "grayscale(0%)";
+  }
+}
+
+function addAll() {
+  jobList = Object.keys(jobData);
+  for (job of jobList) {
+    document.getElementById(job).style.filter = "grayscale(0%)";
+  }
+}
+
+function clearAll() {
+  for (job of jobList) {
+    document.getElementById(job).style.filter = "grayscale(100%)";
+  }
+  jobList = [];
+}
+
+function generateJob() {
+  const random = Math.floor(Math.random() * jobList.length);
+  const chosenJob = jobList[random];
+  console.log(chosenJob);
+  if (chosenJob) {
+    document.getElementById("jobTitle").innerHTML = jobTitle[chosenJob] + "!";
+    document.getElementById("jobImage").src = "images/" + chosenJob + ".png";
+  } else {
+    document.getElementById("jobTitle").innerHTML =
+      "You haven't selected any jobs.";
+    document.getElementById("jobImage").src = "images/allrounder.png";
+  }
+}
